ba0b0b30c71eec8e3788b648ce07bd99dbfa9337
bartosz.bury <bartosz.bury@payeye.com>
2023-01-21 01:44:06 +0100

    move next file to lib

---
+++ src/RequestModel/OrderUpdateStatusRequestModel.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PayEye\Lib\RequestModel;
+
+class OrderUpdateStatusRequestModel
+{
+    /** @var string */
+    private $orderId;
+
+    /** @var string */
+    private $status;
+
+    public function __construct(array $request)
+    {
+        $this->orderId = $request['orderId'];
+        $this->status = $request['status'];
+    }
+
+    public function getOrderId(): string
+    {
+        return $this->orderId;
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+}
---
+++ src/ResponseModel/OrderResponseModel.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PayEye\Lib\ResponseModel;
+
+use PayEye\Lib\Tool\Builder;
+
+class OrderResponseModel
+{
+    use Builder;
+
+    /** @var string */
+    public $checkoutUrl;
+
+    /** @var int */
+    public $orderId;
+
+    /** @var int|float */
+    public $totalAmount;
+
+    /** @var int|float */
+    public $cartAmount;
+
+    /** @var int|float */
+    public $shippingAmount;
+
+    /** @var string */
+    public $currency;
+
+    /** @var string[] */
+    public $signatureFrom;
+
+    public static function createFromArray(array $context): self
+    {
+        return self::builder()
+            ->setCheckoutUrl($context['checkoutUrl'])
+            ->setOrderId($context['orderId'])
+            ->setTotalAmount($context['totalAmount'])
+            ->setCartAmount($context['cartAmount'])
+            ->setShippingAmount($context['shippingAmount'])
+            ->setCurrency($context['currency'])
+            ->setSignatureFrom($context['signatureFrom']);
+    }
+
+    public function setCheckoutUrl(string $checkoutUrl): self
+    {
+        $this->checkoutUrl = $checkoutUrl;
+        return $this;
+    }
+
+    public function setOrderId(int $orderId): self
+    {
+        $this->orderId = $orderId;
+        return $this;
+    }
+
+    /**
+     * @param float|int $totalAmount
+     */
+    public function setTotalAmount($totalAmount): self
+    {
+        $this->totalAmount = $totalAmount;
+        return $this;
+    }
+
+    /**
+     * @param float|int $cartAmount
+     */
+    public function setCartAmount($cartAmount): self
+    {
+        $this->cartAmount = $cartAmount;
+        return $this;
+    }
+
+    /**
+     * @param float|int $shippingAmount
+     */
+    public function setShippingAmount($shippingAmount): self
+    {
+        $this->shippingAmount = $shippingAmount;
+        return $this;
+    }
+
+    public function setCurrency(string $currency): self
+    {
+        $this->currency = $currency;
+        return $this;
+    }
+
+    /**
+     * @param string[] $signatureFrom
+     */
+    public function setSignatureFrom(array $signatureFrom): self
+    {
+        $this->signatureFrom = $signatureFrom;
+        return $this;
+    }
+}
