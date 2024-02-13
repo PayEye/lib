1051ad93187c92d4a51a467ab5dbdbb70ba20b0b
Mark Kantsevich <mark.kantsevich@payeye.com>
2024-02-13 09:40:17 +0000

    Develop

--- src/HttpClient/Model/PluginStatusRequest.php
+++ src/HttpClient/Model/PluginStatusRequest.php
@@ -10,4 +10,29 @@ class PluginStatusRequest
 {
     use Builder;
     use PluginStatusTrait;
+
+    public static function create(
+        int $apiVersion
+        , string $shopIdentifier
+        , string $pluginMode
+        , string $languageVersion
+        , string $platformVersion
+        , string $pluginVersion
+        , string $pluginEvent
+        , ?array $pluginConfig
+    ): self
+    {
+        $object = new self();
+
+        $object->apiVersion = $apiVersion;
+        $object->shopIdentifier = $shopIdentifier;
+        $object->pluginMode = $pluginMode;
+        $object->languageVersion = $languageVersion;
+        $object->platformVersion = $platformVersion;
+        $object->pluginVersion = $pluginVersion;
+        $object->pluginEvent = $pluginEvent;
+        $object->pluginConfig = $pluginConfig;
+
+        return $object;
+    }
 }
