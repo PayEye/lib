93124e1d0ccf9dd4abf94aa222bccf65631274d6
adam.zagroba <adam.zagroba@payeye.com>
2024-02-21 15:59:23 +0100

    Enums for On Click widget settings

---
+++ src/Enum/WidgetModes.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PayEye\Lib\Enum;
+
+abstract class WidgetModes
+{
+    public const FLOATING = 'FLOATING';
+    public const ON_CLICK = 'ON_CLICK';
+}
